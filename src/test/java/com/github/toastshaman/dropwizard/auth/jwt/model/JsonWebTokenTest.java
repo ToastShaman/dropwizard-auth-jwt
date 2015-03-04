0c91c46a7f70a6d298974b506beb7e213a61f5d1
Kevin <kevin.denver@gmail.com>
2015-03-04 10:01:31 +0000

    test(sub): added a test provided by tzahari for the subject property

--- src/test/java/com/github/toastshaman/dropwizard/auth/jwt/model/JsonWebTokenTest.java
+++ src/test/java/com/github/toastshaman/dropwizard/auth/jwt/model/JsonWebTokenTest.java
@@ -34,4 +34,31 @@ public class JsonWebTokenTest {
 
         assertThat(encodedToken, equalTo(expected));
     }
+
+    @Test
+    public void builds_a_valid_token_with_all_claim_fiedls() {
+        final String expected = ""
+                + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+                + ".eyJpc3MiOiJqb2UiLCJleHAiOjEzMDA4MTkzODAsImlhdCI6MTMwMDgxOTMyMCwibmJmIjoxMzAwODE5MzIxLCJzdWIiOiJzdWIiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZX0"
+                + ".H20YqViCitX1Hb_iOP2sqCMMg3oan6KNNd-9gpzjUYI";
+
+
+        final byte[] key = fromBase64("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow");
+
+        JsonWebToken token = JsonWebToken.builder()
+                .header(JsonWebTokenHeader.HS256())
+                .claim(JsonWebTokenClaim.builder()
+                                .issuer("joe")
+                                .subject("sub")
+                                .expiration(DateTime.now().withMillis(1300819380000L))
+                                .issuedAt(DateTime.now().withMillis(1300819320000L))
+                                .notBefore(DateTime.now().withMillis(1300819321000L))
+                                .param("http://example.com/is_root", true)
+                                .build()
+                ).build();
+
+        final String encodedToken = new HmacSHA256Signer(key).sign(token);
+
+        assertThat(encodedToken, equalTo(expected));
+    }
 }
